810c9258935d523f58aa1bc2282cd16e92ab5419
Egorick1845 <proegorweb@gmail.com>
2025-07-19 20:57:13 +0300

    Update CosmicCultRuleSystem.cs

--- Content.Server/_DV/CosmicCult/CosmicCultRuleSystem.cs
+++ Content.Server/_DV/CosmicCult/CosmicCultRuleSystem.cs
@@ -96,6 +96,7 @@ public sealed class CosmicCultRuleSystem : GameRuleSystem<CosmicCultRuleComponen
     [Dependency] private readonly SharedTransformSystem _transform = default!;
     [Dependency] private readonly SharedUserInterfaceSystem _ui = default!;
     [Dependency] private readonly VisibilitySystem _visibility = default!;
+    [Dependency] private readonly ISharedPlayerManager _player = default!;
     private ISawmill _sawmill = default!;
     private TimeSpan _t3RevealDelay = default!;
     private TimeSpan _t2RevealDelay = default!;
@@ -610,7 +611,7 @@ public sealed class CosmicCultRuleSystem : GameRuleSystem<CosmicCultRuleComponen
         radio.Channels.Add("CosmicRadio");
         transmitter.Channels.Add("CosmicRadio");
 
-        if (_mind.TryGetSession(mindId, out var session))
+        if (_player.TryGetSessionById(mind.UserId, out var session))
         {
             _euiMan.OpenEui(new CosmicRoundStartEui(), session);
         }
@@ -718,7 +719,7 @@ public sealed class CosmicCultRuleSystem : GameRuleSystem<CosmicCultRuleComponen
         _mind.TryAddObjective(mindId, mind, "CosmicMonumentObjective");
         _mind.TryAddObjective(mindId, mind, "CosmicEntropyObjective");
 
-        if (_mind.TryGetSession(mindId, out var session))
+        if (_player.TryGetSessionById(mind.UserId, out var session))
             _euiMan.OpenEui(new CosmicConvertedEui(), session);
 
         //RemComp<BibleUserComponent>(uid);
@@ -760,13 +761,13 @@ public sealed class CosmicCultRuleSystem : GameRuleSystem<CosmicCultRuleComponen
             return;
 
         _mind.ClearObjectives(mindId, mindComp);
-        _role.MindTryRemoveRole<CosmicCultRoleComponent>(mindId);
-        _role.MindTryRemoveRole<RoleBriefingComponent>(mindId);
+        _role.MindRemoveRole<CosmicCultRoleComponent>(mindId);
+        _role.MindRemoveRole<RoleBriefingComponent>(mindId);
 
         if (TryComp(uid, out EyeComponent? eyeComp))
             _eye.SetVisibilityMask(uid, eyeComp.VisibilityMask & (int) ~VisibilityFlags.CosmicCultMonument);
 
-        if (_mind.TryGetSession(mindId, out var session))
+        if (_player.TryGetSessionById(mind.UserId, out var session))
             _euiMan.OpenEui(new CosmicDeconvertedEui(), session);
 
         _eye.SetVisibilityMask(uid, 1);
